6843ee91660347990765b0811bf456a50b60aaf6
Дима Олейник <dimaoliynyk@MacBook-Air-Dima-2.local>
2020-12-31 22:52:01 +0200

    fx

---
+++ index.js
@@ -0,0 +1,66 @@
+$(function() {
+  // при нажатии на кнопку scrollup
+  $('#home').click(function() {
+    // переместиться в верхнюю часть страницы
+    $("html, body").animate({
+      scrollTop: 0
+    },1000);
+  })
+});
+$(function() {
+  // при нажатии на кнопку scrollup
+  $('#features').click(function() {
+    // переместиться в верхнюю часть страницы
+    $("html, body").animate({
+      scrollTop: 640
+    },1000);
+  })
+});
+$(function() {
+  // при нажатии на кнопку scrollup
+  $('#shop').click(function() {
+    // переместиться в верхнюю часть страницы
+    $("html, body").animate({
+      scrollTop: 1400
+    },1000);
+  })
+});
+$(function() {
+  // при нажатии на кнопку scrollup
+  $('#blog').click(function() {
+    // переместиться в верхнюю часть страницы
+    $("html, body").animate({
+      scrollTop: 2100
+    },1000);
+  })
+});
+$(function() {
+  // при нажатии на кнопку scrollup
+  $('#work').click(function() {
+    // переместиться в верхнюю часть страницы
+    $("html, body").animate({
+      scrollTop: 1400
+    },1000);
+  })
+});
+$(function() {
+  // при нажатии на кнопку scrollup
+  $('#contact').click(function() {
+    // переместиться в верхнюю часть страницы
+    $("html, body").animate({
+      scrollTop: 3000
+    },1000);
+  })
+});
+
+$(function() {
+  // при нажатии на кнопку scrollup
+  $('.welcome_btn').click(function() {
+    // переместиться в верхнюю часть страницы
+    $("html, body").animate({
+      scrollTop: 1400
+    },1000);
+  })
+});
+
+console.log('ok')
